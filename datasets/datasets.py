6889c9d3792fd5f256303ddc0a4e12db0f742ee7
capDoYeonLee <ghtyu21@naver.com>
2022-01-19 00:11:11 +0900

    ~ing

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -3,24 +3,11 @@ import xml
 from tqdm import tqdm
 from xml.etree.ElementTree import parse
 
-# train_files = sorted(glob('car_plate_datasets\annotations\*'))
-# test_files  = sorted(glob('../car_plate_datasets/'))
-
-# tree = parse('test.xml')
-# root = tree.getroot()
-
-train_file = glob('car_plate_datasets\annotations\*')
-print(train_file[:11])
-
-
-
-
-'''
-train_xml_list = []
-for file in tqdm(train_files[:10]):
-    with open(file, "r") as xml_file:
-        train_xml_list.append(xml.load(xml_file))
-        
-print(train_xml_list)
-
-'''
+doc = sorted(glob("car_plate_datasets\annotations\*"))
+bbox_list = []
+for i in doc:
+  doc = parse(i)
+#root 노드 가져오기
+  root = doc.getroot()
+  object_tag = root.find("object").find("bndbox")
+  bbox_list.append(int(object_tag.findtext("xmin", "ymin")))
